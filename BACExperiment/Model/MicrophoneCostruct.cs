a695225fa8c7ff831669f0d03da02b45800d2804
Iustinian Olaru <o.iustin@gmail.com>
2015-09-20 20:43:17 +0200

    wsaffsafa

---
+++ BACExperiment/Model/MicrophoneCostruct.cs
@@ -0,0 +1,108 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACExperiment.Model
+{
+    public class MicrophoneConstruct
+    {
+        private WaveIn waveIn = null;
+        public  SampleAggregator aggregator { get; set; }
+        private WaveFileWriter file = null;
+        private int sampleCount = 0;
+        public int groupBoxIndex { get; set; }
+
+        public MicrophoneConstruct(int selectedDevice, int groupBoxIndex)
+        {
+            this.groupBoxIndex = groupBoxIndex;
+            waveIn = new WaveIn();
+
+            if(WaveIn.DeviceCount >1)
+            waveIn.DeviceNumber = selectedDevice +1;
+
+            waveIn.WaveFormat = new WaveFormat(44100, 2);
+
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.RecordingStopped += new EventHandler<StoppedEventArgs>(waveIn_RecordingStoped);
+
+            aggregator = new SampleAggregator();
+            aggregator.NotificationCount = 100; // How often we update the volume bar value;
+        }
+
+        public void Listen()
+        {
+            waveIn.StartRecording();
+        }
+
+        public void Record()
+        {
+            string Path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string directoryName = "\\Audio recordings";
+            string fileName = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")+"_"+waveIn.DeviceNumber.ToString()+".wav";
+
+            if (!Directory.Exists(string.Concat(Path , directoryName)))
+                Directory.CreateDirectory(string.Concat(Path, directoryName));
+
+
+            file = new WaveFileWriter(Path+directoryName+"\\"+fileName, waveIn.WaveFormat);
+        }
+
+        public void Stop()
+        {
+            waveIn.StopRecording();
+        }
+
+        public void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (file != null)
+            {
+                file.Write(e.Buffer, 0, e.BytesRecorded);
+                file.Flush();
+            }
+
+
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((e.Buffer[index + 1] << 8) |
+                                        e.Buffer[index + 0]);
+                float sample32 = sample / 32768f;
+
+                ProcessSample(sample32);
+            }
+        }
+
+        public void ProcessSample(float sample)
+        {
+            sampleCount++;
+            if (sampleCount > 800)
+            {
+                aggregator.Add(sample);
+                sampleCount = 0;
+            }
+
+        }
+
+        public void waveIn_RecordingStoped(object sender, StoppedEventArgs e)
+        {
+
+            if (waveIn != null)
+            {
+                waveIn.Dispose();
+                waveIn = null;
+            }
+
+            if (file != null)
+            {
+                file.Dispose();
+                file = null;
+            }
+
+        }
+
+
+    }
+}
---
+++ BACExperiment/Model/MicrophoneHandler.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+using System.IO;
+using System.Diagnostics;
+
+namespace BACExperiment.Model
+{
+    class MicrophoneHandler
+    {
+
+      
+        private List<MicrophoneConstruct> microphones = new List<MicrophoneConstruct>();
+      
+        private Service observer;
+
+        public MicrophoneHandler(Service observer)
+        {
+            this.observer = observer;
+            AudioSessionLogDirectoryPresent(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().ToString()));
+        }
+
+        public List<WaveInCapabilities> MicrophoneList()
+        {
+            int WaveInDevices = WaveIn.DeviceCount;
+            List<WaveInCapabilities> ToReturn = new List<WaveInCapabilities>();
+            for (int WaveInDevice = 0; WaveInDevice < WaveInDevices; WaveInDevice++)
+            {
+                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(WaveInDevice);
+                ToReturn.Add(deviceInfo);
+
+            }
+
+            return ToReturn;
+        }
+
+
+        public void ListenToMicrophone(int selectedDevice , int groupBoxIndex)
+        {
+            MicrophoneConstruct mic = new MicrophoneConstruct(selectedDevice, groupBoxIndex);
+            mic.aggregator.MaximumCalculated += new EventHandler<MaxSampleEventArgs>(MaximimumCalculated);
+
+            if (!(microphones.Count>1))
+            {
+                mic.Listen();
+                microphones.Add(mic);
+            }
+            else
+            {
+                mic.Listen();
+                microphones[groupBoxIndex] = mic;
+            }
+        }
+
+        private void MaximimumCalculated(object sender, MaxSampleEventArgs e)
+        {
+            //Figure ot to which structure the sending aggregator belongs to so that we can update the group box of that specific Micrcophone
+
+            int index = 0;
+            foreach (var x in microphones)
+                if ( x.aggregator == (SampleAggregator)sender)
+                {
+                    index = x.groupBoxIndex;
+                }
+            observer.UpdateVolumeBar( index , Math.Max(e.MaxSample, Math.Abs(e.MinSample)));
+        }
+
+        
+        public void AudioSessionLogDirectoryPresent(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath + "\\Audio Recordings"))
+            {
+                Directory.CreateDirectory(directoryPath + "\\Audio Recordings");
+            }
+
+        }
+
+       
+
+        public static void ConvertToMp3(string lameExePath, string waveFile, string mp3File)
+        {
+            Process converter = Process.Start(lameExePath, "-V2 \"" + waveFile
+                                     + "\" \"" + mp3File + "\"");
+            converter.WaitForExit();
+        }
+
+        internal void StartRecording(int i)
+        {
+            microphones[i-1].Record();
+        }
+
+        internal void StopRecording(int i)
+        {
+            microphones[i-1].Stop();
+        }
+    }
+}
